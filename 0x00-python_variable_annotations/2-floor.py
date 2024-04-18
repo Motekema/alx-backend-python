95b59c118c5c5aa64a5031cc94f4b02ee108b9d7
Motekema <motekema@gmail.com>
2024-04-18 11:25:34 -0700

    My commit

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module for floor function
+"""
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a float number.
+    """
+    return math.floor(n)

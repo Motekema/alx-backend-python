03b9b1271e78bb02ca5991bf9ebf0e9a7e63ff61
Motekema <motekema@gmail.com>
2024-04-18 11:19:17 -0700

    My commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module for add function
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floats and return the result as a float.
+    """
+    return a + b

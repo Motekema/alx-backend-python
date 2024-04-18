28a7e0380a28d286d05f14a716213403a0e50630
Motekema <motekema@gmail.com>
2024-04-18 11:30:41 -0700

    My commit

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module for to_str function
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Return the string representation of a float number.
+    """
+    return str(n)

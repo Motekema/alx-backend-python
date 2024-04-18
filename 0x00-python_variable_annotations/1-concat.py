a26150133dae372ce869348ce61c2016b6b981e9
Motekema <motekema@gmail.com>
2024-04-18 11:23:07 -0700

    My commit

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module for concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings and return the result as a string.
+    """
+    return str1 + str2

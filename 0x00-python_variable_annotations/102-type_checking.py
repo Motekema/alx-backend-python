d2d36c107b19a63a5188cf7d0a4b2acee710f4f0
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-04-19 21:15:18 +0200

    Update 102-type_checking.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,25 +5,17 @@ Module for type checking with mypy.
 from typing import List, Tuple
 
 
-def zoom_array(lst: List, factor: int = 2) -> Tuple:
-    """
-    Zooms in an array by repeating each element a specified number of times.
-
-    Args:
-        lst (List): The list to zoom in.
-        factor (int, optional): The factor by which to zoom in. Defaults to 2.
-
-    Returns:
-        Tuple: The zoomed-in array.
-    """
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    '''Create multiple copies of items in a tuple.
+    '''
     zoomed_in: List = [
         item for item in lst
-        for i in range(factor)
+        for i in range(int(factor))
     ]
-    return tuple(zoomed_in)
+    return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 

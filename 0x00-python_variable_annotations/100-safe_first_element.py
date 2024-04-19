a71bca4e08e0770ae9addbf5072d44d17f1ed209
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-04-19 21:06:27 +0200

    Update 100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 """
-Module for annotating a function that safely retrieves the first element
+Module for annotating a function that safely retrieves the first element.
 """
 from typing import Sequence, Any, Union
 
-
-def safe_first_element(lst: Sequence) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
     Safely retrieves the first element of a sequence.
 
     Args:
-        lst (Sequence): A sequence (e.g., list, tuple, string).
+        lst (Sequence[Any]): A sequence (e.g., list, tuple, string).
 
     Returns:
         Union[Any, None]: The first element of the sequence if the sequence
+        is not empty, otherwise None.
     """
     if lst:
         return lst[0]

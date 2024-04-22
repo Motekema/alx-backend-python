40a548b685f2fdc2fbcf8406a1ab580c8efe2e7e
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-04-22 17:40:17 +0200

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,59 +1,19 @@
 #!/usr/bin/env python3
 """
-This module contains a function to create an asyncio
+This module contains a function to create an async
 """
 
 import asyncio
-from typing import List, Union
-from random import uniform
+from typing import List
 
 
-async def wait_random(max_delay: Union[int, float] = 10) -> float:
-    """
-    Asynchronous coroutine that waits for a random delay between 0 
-
-    Args:
-        max_delay (Union[int, float]): The maximum delay in seconds.
-
-    Returns:
-        float: The random delay.
-    """
-    delay = uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
-
-
-def task_wait_random(max_delay: Union[int, float] = 10) -> asyncio.Task:
-    """
-    Create an asyncio.Task that runs the wait_random coroutine
-
-    Args:
-        max_delay (Union[int, float]): The maximum delay in seconds.
-
-    Returns:
-        asyncio.Task: The asyncio.Task object.
-    """
-    return asyncio.create_task(wait_random(max_delay))
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    Asynchronous routine that spawns task_wait_random n 
-
-    Args:
-        n (int): The number of times to spawn task_wait_random.
-        max_delay (int): The maximum delay in seconds.
-
-    Returns:
-        List[float]: A list of all the delays in ascending order.
-    """
-    delays = [task_wait_random(max_delay) for _ in range(n)]
-    return sorted(await asyncio.gather(*delays))
-
-
-if __name__ == "__main__":
-    import asyncio
-
-    n = 5
-    max_delay = 6
-    print(asyncio.run(task_wait_n(n, max_delay)))
+    '''Execute task_wait_random n times.
+    '''
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
